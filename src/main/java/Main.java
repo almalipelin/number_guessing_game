0295726eba4ab0b6f2a9c23eba83ddcefdf90971
Pelin Almalı <148381211+almalipelin@users.noreply.github.com>
2024-03-14 09:59:30 +0000

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 public class Main{
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     Random a = new Random(); 
     int tahmin,i=0,x;
+    x=(int)(Math.random() * 100)+1;
     do{
       System.out.println("Enter a number between 0 and 100);
       tahmin = scanner.nextInt();
-      x=math.random()*100;
+      
       a.game(x,tahmin); 
       i++;
     }while(tahmin!=x);
